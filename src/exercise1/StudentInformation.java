d934a0773e7f7716550a423af199c175c1ddc391
Gaganbasra55 <38173348+Gaganbasra55@users.noreply.github.com>
2018-07-13 14:28:57 -0400

    exercise 1 working with few changes pending

--- src/exercise1/StudentInformation.java
+++ src/exercise1/StudentInformation.java
@@ -5,15 +5,14 @@ import javax.swing.JFrame;
 public class StudentInformation {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
 
-        // Creating object to display frame
+        // object displaying frame
         StudentInformationFrame studentInformationFrame = new StudentInformationFrame();
         studentInformationFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        // Setting frame size
-        studentInformationFrame.setSize(900, 400);
         // Setting frame visibility
         studentInformationFrame.setVisible(true);
+        // Setting frame size
+        studentInformationFrame.setSize(900, 400);
         // Setting frame display = center
         studentInformationFrame.setLocationRelativeTo(null);
 
